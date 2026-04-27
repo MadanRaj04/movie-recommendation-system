cba996aad4ff44cc84fd7cd9230d30c2cc490329
Madan Raj M A <madanrajma.22aid@kongu.edu>
2026-04-27 23:44:22 +0530

    Simplify database connection arguments
    Removed check for SQLite in database connection arguments.

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -7,7 +7,7 @@ DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+    connect_args= {}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
